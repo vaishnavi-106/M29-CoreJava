b1fa8ee7f1b486f74fbc9e64754f77e428bd6fe5
vaishnavi-12340 <vaishnavi1062@gmail.com>
2023-04-05 18:04:16 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.tnsif.introduction;
+// program to demonstrate simple java program
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
