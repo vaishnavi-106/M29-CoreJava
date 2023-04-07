6b04d008a4d0a1fcc358457e3469382f4e9084bf
vaishnavi-106 <vaishnavib1006@gmail.com>
2023-04-07 11:41:48 +0530

    Second commit

---
+++ dayone/src/org/tnsif/introduction/DataType.java
@@ -0,0 +1,13 @@
+package org.tnsif.introduction;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		int a=5;
+		System.out.println("number is : "+a);
+		float d =105/7;
+		System.out.println("number is : "+d);
+
+	}
+
+}
---
+++ dayone/src/org/tnsif/introduction/PrimitiveDataTypes.java
@@ -0,0 +1,14 @@
+package org.tnsif.introduction;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		long x = 2356789008L;
+		System.out.println(x);
+		
+		byte y = -125;
+		System.out.println(y);
+
+	}
+
+}
