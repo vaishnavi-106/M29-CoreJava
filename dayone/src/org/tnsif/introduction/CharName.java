b7244040762c522dd2445c56b7b78eff9149c188
vaishnavi-106 <vaishnavib1006@gmail.com>
2023-04-08 15:44:05 +0530

    Third Commit

---
+++ dayone/src/org/tnsif/introduction/CharName.java
@@ -0,0 +1,13 @@
+package org.tnsif.introduction;
+
+public class CharName {
+
+	public static void main(String[] args) {
+		char ch ='s';
+		System.out.println(ch);
+		char ch2 =65;
+		System.out.println(ch2);
+
+	}
+
+}
